cf8cef13fe5afd73cf601bf245dec3c28a13ca57
landrystation <t-hamamoto@yabusame-net.com>
2026-06-25 14:55:49 +0900

    Update page.js

--- app/live2d/page.js
+++ app/live2d/page.js
@@ -8,7 +8,7 @@ export default function Live2DPage() {
       try {
         await loadScript("https://cubism.live2d.com/sdk-web/cubismcore/live2dcubismcore.min.js");
         await loadScript("https://cdn.jsdelivr.net/npm/pixi.js@6.5.10/dist/browser/pixi.min.js");
-        await loadScript("https://cdn.jsdelivr.net/npm/pixi-live2d-display@0.4.0/dist/cubism4.min.js");
+        await loadScript("https://cdn.jsdelivr.net/npm/pixi-live2d-display@0.4.1/dist/cubism4.min.js");
 
         const PIXI = window.PIXI;
 
